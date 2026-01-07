3b66edea2cb04a38135aecad0d955f517e083c86
DarshilGoyani <darshilgoyani05@gmail.com>
2026-01-07 15:35:55 +0530

    backend delete

--- tailor-backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const protect = require('../middleware/authMiddleware');
-const { getStats } = require('../controllers/dashboardController');
-
-router.get('/stats', protect, getStats);
-
-module.exports = router;
